e481926cd2478beb24d808f5cecfbb8c212d3cde
Yukesh-30 <yukesh2310573@ssn.edu.in>
2025-05-30 11:17:47 +0530

    frontend routes completed

--- frontend/taskmanager/src/App.jsx
+++ frontend/taskmanager/src/App.jsx
@@ -1,4 +1,17 @@
 import React from "react"
+import { BrowserRouter as Router,
+  Routes,
+  Route } from "react-router-dom"
+import Login from "./pages/auth/Login"
+import SignUp from "./pages/auth/SignUp"
+import PrivateRoute from './routes/PrivateRoute'
+import Dashboard from "./pages/admin/Dashboard"
+import CreateTask from "./pages/admin/CreateTask"
+import ManageTask from "./pages/admin/ManageTask"
+import ManageUsers from "./pages/admin/ManageUsers"
+import MyTask from "./pages/User/MyTask"
+import ViewTask from "./pages/User/ViewTask"
+import UserDashboard from "./pages/User/UserDashboard"
 
 
 
@@ -6,7 +19,31 @@ function App() {
 
   return (
     <>
-      
+
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+
+          <Route element={<PrivateRoute allowedRole={["admin"]}/>}>
+            <Route path="/admin/dashboard" element={<Dashboard/>}/>
+            <Route path="/admin/createtask" element={<CreateTask/>}/>
+            <Route path="/admin/managatask" element={<ManageTask/>}/>
+            <Route path="/admin/managauser" element={<ManageUsers/>}/>
+            
+          </Route>
+
+          <Route element={<PrivateRoute allowedRole={["user"]}/>}>
+            <Route path="/user/mytask" element={<MyTask/>}/>
+            <Route path="/user/viewtask" element={<ViewTask/>}/>
+            <Route path="/user/userdashboard" element={<UserDashboard/>}/>
+
+            
+
+          </Route>
+          
+        </Routes>
+      </Router>
     </>
   )
 }
